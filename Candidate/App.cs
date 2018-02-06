4f83aaebe6f6b465a6b89c51f03eb6b0e8bbeb93
UnreachableCode <cfin@protonmail.com>
2018-02-06 22:17:40 +0000

    Update App.cs namespace in core project

--- Candidate/App.cs
+++ Candidate/App.cs
@@ -1,18 +1,18 @@
 using MvvmCross.Core.ViewModels;
 using MvvmCross.Platform.IoC;
 
-namespace Mobile.Core
+namespace Candidate.Core
 {
-	public class App : MvxApplication
-	{
-		public override void Initialize()
-		{
-			CreatableTypes()
-				.EndingWith("Service")
-				.AsInterfaces()
-				.RegisterAsLazySingleton();
-				
-			RegisterAppStart<ViewModels.FirstViewModel>();
-		}
-	}
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            RegisterAppStart<ViewModels.LoginViewModel>();
+        }
+    }
 }
