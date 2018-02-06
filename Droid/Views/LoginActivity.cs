5cdcabe50ff2c505cf83682e1b1359e4bfb25c19
UnreachableCode <cfin@protonmail.com>
2018-02-06 21:37:24 +0000

    Add LoginActivity and corresponding View

--- Droid/Views/LoginActivity.cs
+++ Droid/Views/LoginActivity.cs
@@ -1,26 +1,27 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
+﻿using Android.App;
 using Android.Widget;
+using Android.OS;
 
 namespace Candidate.Droid.Views
 {
-    [Activity(Label = "LoginActivity")]
-    public class LoginActivity : Activity
+    [Activity(Label = "Candidate", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
     {
+        int count = 1;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            // Create your application here
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.myButton);
+
+            button.Click += delegate { button.Text = $"{count++} clicks!"; };
         }
     }
 }
+
