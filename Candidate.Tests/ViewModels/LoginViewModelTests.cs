bd3e60119b933bf35d2672cc73f2da2f805bcd49
UnreachableCode <cfin@protonmail.com>
2018-02-07 22:11:05 +0000

    Add LoginViewModelTests

---
+++ Candidate.Tests/ViewModels/LoginViewModelTests.cs
@@ -0,0 +1,39 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using Candidate.Core.ViewModels;
+using MvvmCross.Core.Navigation;
+
+namespace Candidate.Tests.ViewModels
+{
+    [TestFixture]
+    public class LoginViewModelTests
+    {
+        [Test]
+        public void Login_WhenCalledWithValidCredentials_SetsWrongLoginToFalse()
+        {
+            var _navigationService = Substitute.For<IMvxNavigationService>();
+            var _loginViewModel = new LoginViewModel(_navigationService);
+
+            _loginViewModel.Username = "charlie@me.com";
+            _loginViewModel.Password = "RubbishPassword";
+
+            _loginViewModel.Login();
+
+            Assert.IsFalse(_loginViewModel.WrongLogin);
+        }
+
+        [Test]
+        public void Login_WhenCalledWithInvalidCredentials_SetsWrongLoginToTrue()
+        {
+            var _navigationService = Substitute.For<IMvxNavigationService>();
+            var _loginViewModel = new LoginViewModel(_navigationService);
+
+            _loginViewModel.Username = "WrongUsername";
+            _loginViewModel.Password = "WrongPassword";
+
+            _loginViewModel.Login();
+
+            Assert.IsTrue(_loginViewModel.WrongLogin);
+        }
+    }
+}
