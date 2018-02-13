2b0fb5a27c410b2c8e347a5294c6497731784417
UnreachableCode <cfin@protonmail.com>
2018-02-13 20:49:19 +0000

    Add solution file and tests

---
+++ Candidate.Tests/ViewModels/CandidateResultsViewModelTests.cs
@@ -0,0 +1,17 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using Candidate.Core.ViewModels;
+using MvvmCross.Core.Navigation;
+
+namespace Candidate.Tests.ViewModels
+{
+    [TestFixture]
+    public class CandidateResultsViewModelTests
+    {
+        [Test]
+        public void ResultListView_ShouldHaveEntries_WhenResultListHasElements()
+        {
+
+        }
+    }
+}
